7e36d139bfdb6851b13813348140f4f76323c5e3
aniketkharel <aniketkhareldev@gmail.com>
2023-04-26 14:29:02 +0545

    using flakes ❇

--- flake.nix
+++ flake.nix
@@ -1,3 +1,39 @@
 {
   "description" = "Gita greeter flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+        overlays = [ (import rust-overlay) ];
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        gita-greeter = rustPlatform.buildRustPackage {
+          pname = "gita-greeter";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+      in
+      {
+        packages = {
+          rustPackage = gita-greeter;
+        };
+        defaultPackage = gita-greeter;
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+        };
+      });
 }
